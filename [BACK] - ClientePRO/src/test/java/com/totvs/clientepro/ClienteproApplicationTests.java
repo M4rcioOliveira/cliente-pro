e54c7e50a64b7ba909229ebfc28169527da76150
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2023-12-17 04:09:01 -0300

    first commit

---
+++ [BACK] - ClientePRO/src/test/java/com/totvs/clientepro/ClienteproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totvs.clientepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
